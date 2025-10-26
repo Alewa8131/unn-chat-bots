c3df7f09be6164ba5fc5afb4b36937ed333a728c
alewa <alewa8132@gmail.com>
2025-10-26 18:52:41 +0300

    Handler MessageEcho has been renamed to TextEcho. Added Handler PhotoEcho for messages with photos and their captions.

--- python-dispatcher-bot/bot/__main__.py
+++ python-dispatcher-bot/bot/__main__.py
@@ -1,12 +1,13 @@
 from bot.dispatcher import Dispatcher
-from bot.handlers.message_echo import MessageEcho
+from bot.handlers.message_echo import TextEcho, PhotoEcho
 from bot.long_polling import start_long_polling
 
 
 if __name__ == "__main__":
     try:
         dispatcher = Dispatcher()
-        dispatcher.add_handler(MessageEcho())
+        dispatcher.add_handler(PhotoEcho())
+        dispatcher.add_handler(TextEcho())
         start_long_polling(dispatcher)
     except KeyboardInterrupt:
         print("\nBye!")
--- python-dispatcher-bot/bot/handlers/message_echo.py
+++ python-dispatcher-bot/bot/handlers/message_echo.py
@@ -1,7 +1,7 @@
 from bot.handler import Handler
 import bot.telegram_api_client
 
-class MessageEcho(Handler):
+class TextEcho(Handler):
     def can_handle(self, update: dict) -> bool:
         return "message" in update and "text" in update["message"]
     
@@ -9,6 +9,19 @@ class MessageEcho(Handler):
         bot.telegram_api_client.sendMessage(
         chat_id=update["message"]["chat"]["id"],
         text=update["message"]["text"],
+        )
+
+        return False
+
+class PhotoEcho(Handler):
+    def can_handle(self, update: dict) -> bool:
+        return "message" in update and "photo" in update["message"]
+    
+    def handle(self, update: dict) -> bool: 
+        bot.telegram_api_client.sendPhoto(
+        chat_id=update["message"]["chat"]["id"],
+        caption=update["message"]["caption"],
+        photo=update["message"]["photo"][-1]["file_id"],
         )
 
         return False
