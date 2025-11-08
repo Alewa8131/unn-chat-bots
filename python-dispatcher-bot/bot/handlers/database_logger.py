d0aa4da08d14c569ecb483b4e65ef45718796b8d
alewa <alewa8132@gmail.com>
2025-11-08 22:55:15 +0300

    Added a welcome message when entering the /start command and transferring the user to the state of selecting the name of the pizza. The dispatcher now receives user data and passes it on to handlers

--- python-dispatcher-bot/bot/handlers/database_logger.py
+++ python-dispatcher-bot/bot/handlers/database_logger.py
@@ -2,9 +2,9 @@ from bot.handlers.handler import Handler, HandlerStatus
 import bot.database_client
 
 class DatabaseLogger(Handler):
-    def can_handle(self, update: dict) -> bool:
+    def can_handle(self, update: dict, state: str, order_json: dict) -> bool:
         return "update_id" in update
     
-    def handle(self, update: dict) -> HandlerStatus: 
+    def handle(self, update: dict, state: str, order_json: dict) -> HandlerStatus: 
         bot.database_client.persist_updates([update])
         return HandlerStatus.CONTINUE
--- python-dispatcher-bot/bot/handlers/ensure_user_exists.py
+++ python-dispatcher-bot/bot/handlers/ensure_user_exists.py
@@ -3,10 +3,10 @@ from bot.handlers.handler import Handler, HandlerStatus
 
 
 class EnsureUserExists(Handler):
-    def can_handle(self, update: dict) -> bool: #, state: str, data: dict
+    def can_handle(self, update: dict, state: str, order_json: dict) -> bool:
         return "message" in update and "from" in update["message"]
     
-    def handle(self, update: dict) -> HandlerStatus: 
+    def handle(self, update: dict, state: str, order_json: dict) -> HandlerStatus: 
         telegram_id = update["message"]["from"]["id"]
         bot.database_client.ensure_user_exists(telegram_id)
         return HandlerStatus.CONTINUE
